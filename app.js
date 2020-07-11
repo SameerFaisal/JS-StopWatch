3f18796fd988bd5bbdddae1d8d3e08cc3edbb083
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2020-07-11 20:01:28 +0500

    Stopwatch Complete

---
+++ app.js
@@ -0,0 +1,65 @@
+var msec = 0;
+var sec = 0;
+var min = 0;
+var timer = 0;
+
+var stopWatchEl = document.querySelector('.stopwatch');
+var lapsContainer = document.querySelector('.laps');
+
+function start() {
+
+    if (!timer) {
+        timer = setInterval(run, 10);
+    }
+}
+
+
+function run() {
+    stopWatchEl.textContent = getTimer();
+    msec++;
+
+    if (msec == 100) {
+        msec = 0;
+        sec++;
+    }
+    if (sec == 60) {
+        sec = 0;
+        min++;
+    }
+}
+
+function pause() {
+    stopTimer();
+}
+
+function stop() {
+    clearInterval(timer);
+    timer = false;
+    min = 0, sec = 0, msec = 0;
+
+    stopWatchEl.textContent = getTimer();
+}
+
+
+function stopTimer() {
+    clearInterval(timer);
+    timer = false;
+}
+
+function getTimer() {
+    return (min < 10 ? "0" + min : min) + ':' + (sec < 10 ? "0" + sec : sec) + ':' + (msec < 10 ? "0" + msec : msec);
+}
+
+
+function restart() {
+    stop();
+    start();
+}
+
+function lap() {
+    if (timer) {
+        var li = document.createElement('li');
+        li.innerText = getTimer();
+        lapsContainer.appendChild(li);
+    }
+}
